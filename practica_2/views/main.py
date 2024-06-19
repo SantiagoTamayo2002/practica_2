8acab95ee3bc6b204b260ccfec7ee6074d7dc286
santiago <manuel.tamayo@unl.edu.ec>
2024-06-19 00:31:30 -0500

    clculo de tiempo corregido

--- practica_2/views/main.py
+++ practica_2/views/main.py
@@ -1,44 +1,41 @@
 import sys
 sys.path.append('../')
 
-from controls.tda.linked.linkedList import Linked_List
-from controls.facturaDaoControl import FacturaDaoControl
-from controls.retencionDaoControl import RetencionDaoControl
-import random
-import time
+from random import randint
+from time import time
+from controls.tda.linked.order.quickSort import QuickSort
+from controls.tda.linked.order.shellSort import ShellSort
+from controls.tda.linked.order.mergeSort import MergeSort
 
-listaNumber = Linked_List()
-listaString = Linked_List()
+quicksort = QuickSort()
+shell = ShellSort()
+merge = MergeSort()
 
 list10000numbres = []
 for i in range(0,10000):
     list10000numbres.append(randint(0,10000))
-    
-    inicio = time()
-    listquick = quicksort.sort(list10000numbres)
-    print("---------------------------------------------")
-    print("QuickSort with 10000 numbers: ", time()-inicio)
-    inicio = time()
-    listshell = shell.sort_descendent(list10000numbres)
-    print("ShellSort with 10000 numbers: ", time()-inicio)
-    inicio = time() 
-    listamerge = merge.sort_descendent(list10000numbres)
-    print("MergeSort with 10000 numbers: ", time()-inicio)
-    
-    list20000numbres = []
-    for i in range(0,20000):
-        list20000numbres.append(randint(0,20000))
 
-    inicio = time()
-    listquick = quick.sort_acendent(list20000numbres)
-    print("---------------------------------------------")
-    print("QuickSort with 20000 numbers: ", time()-inicio)
-    inicio = time()
-    listshell = shell.sort_descendent(list20000numbres)
-    print("ShellSort with 20000 numbers: ", time()-inicio)
-    inicio = time() 
-    listamerge = merge.sort_descendent(list20000numbres)
-    print("MergeSort with 20000 numbers: ", time()-inicio)
+inicio = time()
+listquick = quicksort.sort_primitive_ascendent(list10000numbres)
+print("---------------------------------------------")
+print("QuickSort with 10000 numbers: ", time()-inicio)
+inicio = time()
+listshell = shell.sort_primitive_ascendent(list10000numbres)
+print("ShellSort with 10000 numbers: ", time()-inicio)
+inicio = time() 
+listamerge = merge.sort_primitive_descendent(list10000numbres)
+print("MergeSort with 10000 numbers: ", time()-inicio)
 
-    
- 
+list20000numbres = []
+for i in range(0,20000):
+    list20000numbres.append(randint(0,20000))
+inicio = time()
+listquick = quicksort.sort_primitive_descendent(list20000numbres)
+print("---------------------------------------------")
+print("QuickSort with 20000 numbers: ", time()-inicio)
+inicio = time()
+listshell = shell.sort_primitive_descendent(list20000numbres)
+print("ShellSort with 20000 numbers: ", time()-inicio)
+inicio = time() 
+listamerge = merge.sort_primitive_descendent(list20000numbres)
+print("MergeSort with 20000 numbers: ", time()-inicio)
